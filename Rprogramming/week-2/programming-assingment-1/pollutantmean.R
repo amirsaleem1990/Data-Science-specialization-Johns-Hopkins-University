6cb78b2b8db536f343dae538ba7b3d769e92a8e2
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-31 12:21:57 +0500

    updated

--- Rprogramming/week-2/programming-assingment-1/pollutantmean.R
+++ Rprogramming/week-2/programming-assingment-1/pollutantmean.R
@@ -3,6 +3,15 @@ pollutantmean <- function(directory, pollutant, id = 1:332){
 	## 'pollutant' is a character vector of length 1 indicating the name of the pollutant of which we will calculate the mean; eithter 'sulfate' or 'nitrate'
 	## 'id' is an integer vectro indicating the monitor ID numbers to be used
 	## Return the mean of the pollutant across all monitors list in the 'id' vector (ignoring NA values)
-
+	
 	## NOTE: do not round the result!
 }
+
+
+if (pollutantmean("specdata", "nitrate", 70:72) == 1.706047){
+	print("OK")
+}else print("NO")
+
+if (pollutantmean("specdata", "nitrate", 23)) == 1.280833{
+	print("OK")
+}else print("NO")
