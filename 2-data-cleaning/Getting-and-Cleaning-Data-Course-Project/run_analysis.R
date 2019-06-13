3b45e1988901fa442d56ca41f873c591535adff8
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-06-13 22:25:13 +0500

    updated

---
+++ 2-data-cleaning/Getting-and-Cleaning-Data-Course-Project/run_analysis.R
@@ -0,0 +1,25 @@
+official_help <- "https://thoughtfulbloke.wordpress.com/2015/09/09/getting-and-cleaning-the-assignment/"
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(url, destfile = "Dataset.zip")
+library(utils)
+utils::unzip("Dataset.zip")
+# 1- Merges the training and the test sets to create one data set.
+features_names <- read.table("UCI HAR Dataset/features.txt")
+X_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features_names$V2)
+X_train['train/test'] <- "train"
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = c("target"))
+
+X_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features_names$V2)
+X_test['train/test'] <- "test"
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = c("target"))
+
+df <- rbind(cbind(X_train, y_train), cbind(X_test, y_test))
+rm(features_names, X_train, y_train, X_test, y_test)
+# 2- Extracts only the measurements on the mean and standard deviation for each measurement.
+length(grep("mean|std", names(df)))
+# 3- Uses descriptive activity names to name the activities in the data set
+first_element_func <- function(x){x[1]}
+first_element <- strsplit(names(df), "\\.")
+unique_fist <- unique(sapply(first_element, first_element_func))
+# 4- Appropriately labels the data set with descriptive variable names.
+# 5- From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
